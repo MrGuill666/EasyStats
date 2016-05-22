067df1d2c9855bdf18d1148646dc63c7cb511942
MrGuill666 <gergelyszaz@gmail.com>
2016-05-22 17:17:31 +0200

    added viewmodel

---
+++ EasyStats/EasyStats/viewmodel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyStats.model;
+
+namespace EasyStats.viewmodel
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Day = 1;
+            Categories = new List<Category>();
+            var c1 = new Category(){ Unit="kg", CategoryName = "Category1"
+            };
+            var c2 = new Category() {  CategoryName = "Category2" };
+            Categories.Add(c1);
+            Categories.Add(c2);
+            var d1 = new Data() { Day = 1, Category = c1, Value = 6.0 };
+            var d2 = new Data() { Day = 1, Category = c2, Value = 7.0 };
+            c1.DataList.Add(d1);
+            c2.DataList.Add(d2);
+
+
+        }
+
+        public int Day { get; set; }
+        public List<Data> DailyDataList {
+            get
+            {
+                var datas = new List<Data>();
+                foreach(var c in Categories)
+                {
+                    foreach(var d in c.DataList)
+                    {
+                        if (d.Day == Day)
+                        {
+                            datas.Add(d);
+                        }
+                    }
+                }
+                return datas;
+            }
+        }
+        public List<Category> Categories { get; set; }
+    }
+
+}
