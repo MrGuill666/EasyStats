70b42c2d38de200ca47d361af0648f4d59f56b17
MrGuill666 <gergelyszaz@gmail.com>
2016-05-22 16:08:52 +0200

    Model and view classes added.

---
+++ EasyStats/EasyStats/model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyStats.model
+{
+    class Category
+    {
+        public string Name { get; set; }
+        public IList<Data> DataList { get; set; }
+        public string Unit { get; set; }
+    }
+}
---
+++ EasyStats/EasyStats/model/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyStats.model
+{
+    class Data
+    {
+        public double Value { get; set; }
+        public Category Category { get; set; }
+        public string Comment { get; set; }
+    }
+}
